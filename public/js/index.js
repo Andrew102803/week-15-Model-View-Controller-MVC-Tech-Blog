661609075729a86c48eb2f7df17398b590d730d7
Andrew102803 <106626454+Andrew102803@users.noreply.github.com>
2022-11-01 20:43:26 -0500

    Create index.js

---
+++ public/js/index.js
@@ -0,0 +1,514 @@
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tech Blog</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="/css/style.css" />
+		
+  </head>
+
+  <body>
+    <div class="flex-column justify-space-around">
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid navBack">
+          <a class="navbar-brand navTitle" href="/">The Tech Blog</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+                <li>
+                  <a class="nav-link" href="/login">Login</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="/signUp">Sign Up</a>
+                </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <main class="container">
+        <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/2">This is my first Blog</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          Billy
+          on
+          5/24/2021
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        Wow isn&#x27;t this just the best first blog you&#x27;ve ever seen?!
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/9">test</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          Sam1234
+          on
+          3/31/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        asddas
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/10">adfsad</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          sal
+          on
+          4/9/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        adfsdf
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/11">stephen post</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          stepelliott15
+          on
+          4/18/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        shashgasgpeaklflg
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/12">hello</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          test
+          on
+          4/19/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        blog
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/13">first post</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          hero123
+          on
+          4/24/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        first content
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/15">hi</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          hello123
+          on
+          5/7/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        123
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/16">no</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          hello123
+          on
+          5/7/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        yes
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/18">Todays Blog</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          bob@gmail.com
+          on
+          5/8/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        top top
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/19">lets go</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          u765@gmail.com
+          on
+          6/9/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        egfafgf
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/20">Games</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          u765@gmail.com
+          on
+          6/9/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        Warcraft
+Diablo
+Call of duty
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/21">recommendation</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          u765@gmail.com
+          on
+          6/9/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        Call of duty
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/22">the blog</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          bruddah
+          on
+          7/12/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        the blog post
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/23">hello</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          hhello
+          on
+          8/3/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        yummy
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/26">adsfasdf</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          testname
+          on
+          9/28/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        asdfasdfasdf
+      </p>
+    </div>
+    </div>
+
+  </div>
+
+
+  <div class="row">
+
+    <div class="container blogBox">
+			<div class="row  justify-content-center headerRow">
+      <div class="col-8">
+        <h2>
+          <a class="blogTitle text-uppercase" href="/blog/28">hehe</a>
+        </h2>
+      </div>
+
+      <div class="col-4">
+        <p class="blogCreate text-right">Created by
+          lalalla@yahoo.com
+          on
+          10/23/2022
+        </p>
+      </div>
+			</div>
+    <div class="blogDesc">
+      <p class="text-center blogDescMain">
+        hhehehee
+      </p>
+    </div>
+    </div>
+
+  </div>
+      </main>
+      <footer>
+      </footer>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+      crossorigin="anonymous"
+    ></script>
+
+  </body>
+
+</html>
